b8f1da3010cc306a4e44d0a68de1cef5f0703f8b
amabie-mamoru <amabie.mamoru@gmail.com>
2021-08-08 16:45:47 +0900

    add comment

--- Runtime/StringDelegator.cs
+++ Runtime/StringDelegator.cs
@@ -4,8 +4,23 @@ using UnityEditor;
 
 namespace com.amabie.StringDelegator
 {
+    /// <summary>
+    /// 実行したいクラスの Type インスタンスと実行したいメソッドの string を与えて実行する Extension
+    /// NOTE: gameObj に実行したい MonoBehaviour インスタンスが付与されていることを前提としている
+    /// 当初は string だけを渡して実行したかったができなかったので名前だけ名残がある
+    /// Type インスタンスではなく string を引数に与えて、この Extension の中で Type.GetType しようとしても
+    /// パッケージ外部のクラスの Type インスタンスを生成できなかったのでやむなくこの形に。
+    /// </summary>
     public static class StringDelegator
     {
+        /// <summary>
+        /// 引数に指定したメソッドを Func オブジェクトとして返す
+        /// </summary>
+        /// <typeparam name="T">bool, int, string など</typeparam>
+        /// <param name="gameObj">実行したい MonoBehaviour クラスが付与されたゲームオブジェクト</param>
+        /// <param name="classTypeWithNamespace">Type インスタンス(ネームスペースを含む)</param>
+        /// <param name="methodName">実行したいメソッド名</param>
+        /// <returns>Func<T>オブジェクト</returns>
         public static Func<T> ToFunc<T>(this GameObject gameObj, Type classTypeWithNamespace, string methodName)
         {
             var assembly = classTypeWithNamespace.Assembly;
@@ -15,6 +30,13 @@ namespace com.amabie.StringDelegator
             return (Func<T>)Delegate.CreateDelegate(typeof(Func<T>), component, method);
         }
 
+        /// <summary>
+        /// 引数に指定したメソッドを Action オブジェクトとして返す
+        /// </summary>
+        /// <param name="gameObj">実行したい MonoBehaviour クラスが付与されたゲームオブジェクト</param>
+        /// <param name="classTypeWithNamespace">Type インスタンス(ネームスペースを含む)</param>
+        /// <param name="methodName">実行したいメソッド名</param>
+        /// <returns>Actionオブジェクト</returns>
         public static Action ToAction(this GameObject gameObj, Type classTypeWithNamespace, string methodName)
         {
             var assembly = classTypeWithNamespace.Assembly;
